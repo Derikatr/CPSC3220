0f104ee6c911301168c5e9bc130f38db1a8cdfbe
Derikatr <derikatr@gmail.com>
2025-03-11 21:05:23 -0400

    Update homework4.php

--- homework4.php
+++ homework4.php
@@ -1,5 +1,5 @@
 <?php
-$customers= 100;
+$customers = 100;
 $order = 350;
 $product = 750;
 $order_item = 550;
@@ -7,43 +7,42 @@ $address = 250;
 $warehouse = 25;
 $product_warehouse = 1250;
 
-function getAddress($id){
-    $streets= ["Parker Street", '3rd Street', 'Main Street', 'Market Street', 'Walnut Street', 'McKenzie Street', 'Vine Street', 'Oak Street'];
-    $cities= ['Washington'];
-    $states= ['New Jersey', 'Virginia', 'Pennsylvania', 'Mississippi', 'Massachusetts' ];
+$cities = file("cities.txt", FILE_IGNORE_NEW_LINES);
+$email = file("domains.txt", FILE_IGNORE_NEW_LINES);
+$first_names = file("first_names.txt", FILE_IGNORE_NEW_LINES);
+$last_names = file("last_names.txt", FILE_IGNORE_NEW_LINES);
+$products = file("products.txt", FILE_IGNORE_NEW_LINES);
+$states = file("states.txt", FILE_IGNORE_NEW_LINES);
+$streets = file("street_names.txt", FILE_IGNORE_NEW_LINES);
+$streetTypes = file("street_types.txt", FILE_IGNORE_NEW_LINES);
 
-    $zip = rand(00000,99999);
-    $street = $streets[array_rand($streets)] . " " . rand(1,999);
+function getAddress($id, $cities, $streets, $states){
+    $zip = rand(10000, 99999); 
+    $street = rand(1, 999) . $streets[array_rand($streets)] . $streetTypes[array_rand($streetTypes)];
     $city = $cities[array_rand($cities)];
     $state = $states[array_rand($states)];
 
     return "INSERT INTO address (address_id, street, city, state, zip) VALUES ($id, '$street', '$city', '$state', '$zip');";
 }
 
-function getCustomer($id){
-    $first_names = ['Gerald', 'Samantha', "Barbra", "Maria", "Bob", "Trey", "Darion", "Candy"] ;
-    $last_names = ["Smith", "Johnson", "Wilson", "Peterson", "Jefferson", "Jackson"];
-
+function getCustomer($id, $first_names, $last_names, $domains){
     $first_name = $first_names[array_rand($first_names)];
     $last_name = $last_names[array_rand($last_names)];
-    $email = strtolower($first_names[array_rand($first_names)]) . "." . strtolower($last_names[array_rand($last_names)]) . "@".(rand(0, 1) ? "gmail" : "yahoo") . ".com";
+    $email = strtolower($first_name) . "." . strtolower($last_name) . "@" . $domains[array_rand($domains)];
     $phone = sprintf("(%03d) %03d - %04d", rand(100, 999), rand(100, 999), rand(100, 999));
-    $address_id= rand(1,999);
-
+    $address_id = rand(1, 999);
 
     return "INSERT INTO customer (customer_id, first_name, last_name, email, phone, address_id) VALUES ($id, '$first_name', '$last_name', '$email', '$phone', $address_id);";
 }
 
-function getProduct($id){
-    $product_names= ["table", "couch", "dresser", "chair", "bed"];
+function getProduct($id, $products){
+    $product_name = $products[array_rand($products)];
     $descriptions = ["Great for sitting", "Great for eating", "Family Friendly", "Storage"];
-
-    $product_name = $product_names[array_rand($product_names)];
     $description = $descriptions[array_rand($descriptions)];
-    $weight = rand(100, 500);
+    $weight = rand(100, 500); 
     $base_cost = rand(100, 9999);
-
 
     return "INSERT INTO product (product_id, product_name, description, weight, base_cost) VALUES ($id, '$product_name', '$description', $weight, $base_cost);";
 }
+
 ?>
