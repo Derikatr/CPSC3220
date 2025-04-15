99e43c45f4f73aaf812bcb571c0c2918a83d140f
Derikatr <derikatr@gmail.com>
2025-04-15 11:25:32 -0400

    Create view_customer.php

---
+++ view_customer.php
@@ -0,0 +1,40 @@
+<?php
+    include("database.php");
+
+    $sql = "
+SELECT 
+    c.first_name,
+    c.last_name,
+    a.address,
+    ci.city,
+    a.district,
+    a.postal_code,
+    GROUP_CONCAT(DISTINCT f.title ORDER BY f.title SEPARATOR ', ') AS rental
+FROM customer c
+JOIN address a ON c.address_id = a.address_id
+JOIN city ci ON a.city_id = ci.city_id
+JOIN rental r ON c.customer_id = r.customer_id
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
+GROUP BY c.customer_id
+ORDER BY c.last_name, c.first_name;
+";
+   $result= mysqli_query($conn, $sql);
+
+   if(mysqli_num_rows($result) > 0){
+       $row = mysqli_fetch_assoc($result);
+       echo $row["first_name"]." <br>";
+       echo $row["last_name"]. "<br>";
+       echo $row["address"]. "<br>";
+       echo $row["city"]. "<br>";
+       echo $row["district"]. "<br>";
+       echo $row["postal_code"]. "<br>";
+       echo $row["rental"]. "<br>";
+   }
+
+
+
+    mysqli_close($conn);
+
+
+?>
