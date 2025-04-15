2dd9b6ee11ba87dc6d4675d85426cadb5432afe1
ItsRaids <123350734+ItsRaids@users.noreply.github.com>
2025-04-15 14:53:47 -0400

    Update view_customer.php
    Changed DB_name to sakila as given from last assignment.
    added the table formatting to properly display customers
    edited the customer output so it loops and doesnt just run 1 line.
    remove open spaced in db_pass and conn

--- view_customer.php
+++ view_customer.php
@@ -1,10 +1,10 @@
 <?php
    $db_server = "localhost";
     $db_user = "root";
-    $db_pass = " ";
-    $db_name = "sakiladb";
+    $db_pass = "";
+    $db_name = "sakila";
     $port = 3306;
-    $conn = " ";
+    $conn = "";
 
     $conn = mysqli_connect($db_server, $db_user, $db_pass,$db_name, $port);
 
@@ -14,6 +14,7 @@
     else{
         echo "Connection failed";
     }
+$sql = "
 SELECT 
     c.first_name,
     c.last_name,
@@ -32,21 +33,50 @@ GROUP BY c.customer_id
 ORDER BY c.last_name, c.first_name;
 ";
    $result= mysqli_query($conn, $sql);
-
-   if(mysqli_num_rows($result) > 0){
-       $row = mysqli_fetch_assoc($result);
-       echo $row["first_name"]." <br>";
-       echo $row["last_name"]. "<br>";
-       echo $row["address"]. "<br>";
-       echo $row["city"]. "<br>";
-       echo $row["district"]. "<br>";
-       echo $row["postal_code"]. "<br>";
-       echo $row["rental"]. "<br>";
-   }
-
-
-
-    mysqli_close($conn);
-
-
 ?>
+
+<!DOCTYPE html>
+<html>
+   <head>
+         <title>View Customers</title>
+   </head>
+<body>
+      <h1> Customer List</h1>
+   <table border="1" cellpadding="5">
+      <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>District</th>
+            <th>Postal Code</th>
+            <th>Films Rented</th>
+      </tr>
+<?php
+   if(mysqli_num_rows($result) > 0){
+       while ($row = mysqli_fetch_assoc($result)) {
+       echo "<tr>"; 
+       echo "<td>" . htmlspecialchars($row["first_name"])." <br>";
+       echo "<td>" . htmlspecialchars($row["last_name"]). "<br>";
+       echo "<td>" . htmlspecialchars($row["address"]). "<br>";
+       echo "<td>" . htmlspecialchars($row["city"]). "<br>";
+       echo "<td>" . htmlspecialchars($row["district"]). "<br>";
+       echo "<td>" . htmlspecialchars($row["postal_code"]). "<br>";
+       echo "<td>" . htmlspecialchars($row["rental"]). "<br>";
+       echo "</tr>"; 
+      }
+   }   
+      else 
+      {
+         echo "<tr><td colspan='7'>No customers found.</td></tr>";
+      }
+    mysqli_close($conn);
+?>
+      
+</table>  
+<br>
+   <button onclick="window.location.href='manager.html'">Back to Manager Page</button>
+</body>
+</html>
+
+
