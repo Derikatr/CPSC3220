60eed7547179fc6e9d338aab3f7dbd75046a5d3f
jewellittlefield <jewellittlefield23@gmail.com>
2025-04-15 18:27:49 -0400

    Update add_film.php

--- add_film.php
+++ add_film.php
@@ -12,27 +12,42 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+$required_fields = [
+    'title', 'description', 'release_year', 'language_id',
+    'rental_duration', 'rental_rate', 'length',
+    'replacement_cost', 'rating', 'special_features'
+];
+
+foreach ($required_fields as $field) {
+    if (!isset($_POST[$field])) {
+        die("Missing required field: $field");
+    }
+}
+
 
 $title = $_POST['title'];
 $description = $_POST['description'];
-$release_year = $_POST['release_year'];
-$language_id = $_POST['language_id'];
-$rental_duration = $_POST['rental_duration'];
-$rental_rate = $_POST['rental_rate'];
-$length = $_POST['length'];
-$replacement_cost = $_POST['replacement_cost'];
+$release_year = (int)$_POST['release_year'];
+$language_id = (int)$_POST['language_id'];
+$rental_duration = (int)$_POST['rental_duration'];
+$rental_rate = (float)$_POST['rental_rate'];
+$length = (int)$_POST['length'];
+$replacement_cost = (float)$_POST['replacement_cost'];
 $rating = $_POST['rating'];
 $special_features = $_POST['special_features'];
 
 
-$sql = "
-INSERT INTO film 
-(title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating, special_features)
-VALUES 
-('$title', '$description', $release_year, $language_id, $rental_duration, $rental_rate, $length, $replacement_cost, '$rating', '$special_features');
-";
+$stmt = $conn->prepare("
+    INSERT INTO film 
+    (title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating, special_features)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+");
 
-
+$stmt->bind_param("ssiiiiddss", 
+    $title, $description, $release_year, $language_id, 
+    $rental_duration, $rental_rate, $length, 
+    $replacement_cost, $rating, $special_features
+);
 ?>
 <!DOCTYPE html>
 <html>
@@ -42,11 +57,13 @@ VALUES
 <body>
     <h1>Add Film Result</h1>
     <?php
-    if (mysqli_query($conn, $sql)) {
+    if ($stmt->execute()) {
         echo "<p style='color: green;'>Film added successfully!</p>";
     } else {
-        echo "<p style='color: red;'>Error adding film: " . mysqli_error($conn) . "</p>";
+        echo "<p style='color: red;'>Error adding film: " . $stmt->error . "</p>";
     }
+
+    $stmt->close();
     mysqli_close($conn);
     ?>
     <br>
